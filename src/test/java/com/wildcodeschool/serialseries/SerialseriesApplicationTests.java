01b4a6adddd7edd791c364d32ab42ac0ad9477d7
Markus Heinrichs <markus@heinrichs.es>
2019-12-13 16:33:49 +0100

    Initial version from start.spring.io

---
+++ src/test/java/com/wildcodeschool/serialseries/SerialseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.serialseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerialseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
