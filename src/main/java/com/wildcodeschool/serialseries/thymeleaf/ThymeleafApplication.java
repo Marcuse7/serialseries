54a874b091cc21cee73476e6663964e9513d6980
Kadir <abdulkadir.erucu@telekom.de>
2020-02-11 17:22:00 +0100

    Jetzt funzt es.
    Admin ist jetzt hard coded.
    
    Username: admin
    Passwort: admin

--- src/main/java/com/wildcodeschool/serialseries/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/wildcodeschool/serialseries/thymeleaf/ThymeleafApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 @SpringBootApplication
 public class ThymeleafApplication {
 	
