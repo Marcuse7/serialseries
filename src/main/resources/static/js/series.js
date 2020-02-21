a8d41f63954088d244fb92edb180efcd6e6a2d2e
Marcuse7 <markus@heinrichs.es>
2020-02-21 16:51:43 +0100

    Click on Series open detail view.
    Duplicate spring-boot-starter-thymeleaf removed from pom.xml.

--- src/main/resources/static/js/series.js
+++ src/main/resources/static/js/series.js
@@ -6,12 +6,12 @@ function subscription(e) {
 	if (e.checked) {
 	$.ajax({
         url: '/series/subscribe',
-        data: { seriesID: e.getAttribute("data-seriesid") }
+        data: { seriesId: e.getAttribute("data-seriesid") }
     });
 	} else {
 		$.ajax({
 	        url: '/series/unsubscribe',
-	        data: { seriesID: e.getAttribute("data-seriesid") }
+	        data: { seriesId: e.getAttribute("data-seriesid") }
 	    });
 		
 	}
